e178da2c402c1720a805760ada08de3abf66d15b
Jazer Abreu <sokartema@gmail.com>
2014-10-01 23:21:29 +0100

    Añadidas muchas funcionalidades a twitter.rb y twitter.erb

--- test/test.rb
+++ test/test.rb
@@ -1,5 +1,5 @@
 ENV['RACK_ENV'] = 'test'
-require 'twitter.rb'
+require_relative '../twitter.rb'
 require 'test/unit'
 require 'minitest/autorun'
 require 'rack/test'
@@ -10,18 +10,9 @@ def app
   Sinatra::Application
 end
 
-
-#Carga de variables para los test
-
  
-describe "Tests de pagina web y twitter" do
-	before :all do
-
-		@usuario1 = "sokartema"
-		@usuario2 = "nook1etrolltime"
-		@usuario3 = "sokartemafalso"
+describe "Tests de la pagina raiz ('/') con metodo get" do
 	
-	end	
 
 	it "Carga de la web desde el servidor" do
 		get '/'
@@ -33,11 +24,69 @@ describe "Tests de pagina web y twitter" do
 		assert last_response.body.include?("<title>Inicio</title>"), "El titulo tiene que ser: Inicio"
 	end
 
-	it "Comprueba el contenido del cuadro de texto" do
+	it "Comprueba que en la pagina hay una cabecera Most Popular Twitter Friend" do
+
 		get '/'
-		#assert last_response.body.include?("NºAmigos a mostrar (max 10)"), "El contenido tiene que estar en la web"
+		assert last_response.body.include?("Twitter Most Popular Friend"), "El titulo de cabecera tiene que estar en el contenido"
+
 	end
 
+	it "Comprueba el contenido del cuadro de texto" do
+		get '/'
+		assert last_response.body.include?("NºAmigos a mostrar (max 10)"), "El contenido tiene que estar en la web"
+	end
+
+	it "Comprueba el mensaje en el campo de visualizacion de amigos" do
+		get '/'
+		assert last_response.body.include?("No se ha introducido ningun usuario o es incorrecto"), "Tiene que haber un mensaje informativo"
+
+	end	
+
 end
+
+
+describe "Test de la pagina raiz ('/') con metodo post" do
+
+	#Carga de variables para los tests
+	before :all do
+
+		@usuario1 = "sokartema"
+		@usuario2 = "nook1etrolltime"
+		@usuario3 = "sokartemafalso"
+		@number1 = 0
+		@number2 = 5
+		@number3 = 11
+	
+	end	
+
+	it "Comprueba mensaje en campo de visualizacion de amigos" do
+
+		post '/', {:nombre => @usuario1, :numero => @number2}
+
+		assert last_response.body.include?("Los amigos mas populares del usuario #{@usuario1} son:"), "Tiene que mostrarse el mensaje"
+
+
+	end
+
+	it "Comprueba mensaje de error numero mayor en campo visualizacion de amigos" do
+
+		post '/', {:nombre => @usuario2, :numero => @number3}
+
+		assert last_response.body.include?("Error, Sólo se puede mostrar hasta 10 usuarios"), "Tiene que mostrarse el mensaje de error"
+
+	end
+
+	it "Comprueba el mensaje de eror usuario no existe en el campo de visualizacion de amigos" do
+
+		post '/', {:nombre => @usuario3, :numero => @number2}
+
+		assert last_response.body.include?("Error, El usuario no existe o no tiene amigos"), "Tiene que mostrarse el mensaje de error"
+
+	end
+	
+
+
+end
+
 
 
--- twitter.rb
+++ twitter.rb
@@ -1,6 +1,8 @@
 require 'twitter'
 require 'sinatra'
 require './configure'
+require 'json'
+
 
 get '/' do
   @nombreamigos = []
@@ -11,15 +13,74 @@ end
 
 post '/' do
   @nombreamigos = []
+  document = ''
   client = my_twitter_client() 
   @name = params[:nombre] || ''
   @number = params[:numero].to_i
   @number = 1 if @number < 1
   if (client.user? @name) && (@number <= 10)
+
+    if(params[:checkbox] != 'on')
     ultimos_t = client.friends(@name,{:skip_status => 1, :include_user_entities => false})
     @nombreamigos =(@nombreamigos != '') ? ultimos_t.map{ |i| [i.name ,i.followers_count]} : ''
     @nombreamigos.sort_by!{|a,b| b}.reverse!
+
+    document = JSON.pretty_generate([{:name => @name , :number => @number} , @nombreamigos])
+
+    file = File.open("json/#{@name}.json", "w")
+
+    file.write(document)
+
+    file.close
+
+    else
+
+      begin
+
+      file = File.read("json/#{@name}.json")
+
+      rescue
+
+      $stderr.puts "Error al abrir el archivo"
+  
+
+      else
+
+      @array = JSON.parse(file)
+      @nombreamigos = @array[1]
+
     
+       end
+
+    end
+   
   end
   erb :twitter
 end
+
+get '/json/:nombre' do 
+
+  content_type :json
+  document = ''
+  @name = params[:nombre]
+  @array = []
+
+  begin
+
+  file = File.read("json/#{@name}.json")
+
+  rescue
+
+    document = JSON.pretty_generate({:parameters => "No existe el usuario"})
+
+  else
+
+    @array = JSON.parse(file)
+    document = JSON.pretty_generate(@array)
+
+  end
+
+
+end
+
+
