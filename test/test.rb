24cfc627bbcb5bd195f129d5cfbc7d2773a9d66d
Javier C Rodriguez <alu0100505023@ull.edu.es>
2014-10-02 11:35:40 +0100

    Añadiendo tests css e imagenes

--- test/test.rb
+++ test/test.rb
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 ENV['RACK_ENV'] = 'test'
 require_relative '../twitter.rb'
 require 'test/unit'
@@ -12,7 +14,15 @@ end
 
  
 describe "Tests de la pagina raiz ('/') con metodo get" do
-	
+
+	#Carga de variables de contenido para los tests
+	before :all do
+		@imagen="twitter-xxl.png"
+		@textoTitulo="<title>Inicio</title>"
+		@textoCabecera="Twitter Most Popular Friend"
+		@textoContenido="NºAmigos a mostrar (max 10)"
+		@css="../public/css/lavish-bootstrap.css"
+	end
 
 	it "Carga de la web desde el servidor" do
 		get '/'
@@ -21,19 +31,19 @@ describe "Tests de la pagina raiz ('/') con metodo get" do
 	
 	it "Comprueba el titulo de la pagina" do
 		get '/'
-		assert last_response.body.include?("<title>Inicio</title>"), "El titulo tiene que ser: Inicio"
+		assert last_response.body.include?(@textoTitulo), "El titulo tiene que ser: "+@textoTitulo
 	end
 
 	it "Comprueba que en la pagina hay una cabecera Most Popular Twitter Friend" do
 
 		get '/'
-		assert last_response.body.include?("Twitter Most Popular Friend"), "El titulo de cabecera tiene que estar en el contenido"
+		assert last_response.body.include?(@textoCabecera), "El titulo de cabecera tiene que estar en el contenido"
 
 	end
 
 	it "Comprueba el contenido del cuadro de texto" do
 		get '/'
-		assert last_response.body.include?("NºAmigos a mostrar (max 10)"), "El contenido tiene que estar en la web"
+		assert last_response.body.include?(@textoContenido), "El contenido tiene que estar en la web"
 	end
 
 	it "Comprueba el mensaje en el campo de visualizacion de amigos" do
@@ -41,6 +51,15 @@ describe "Tests de la pagina raiz ('/') con metodo get" do
 		assert last_response.body.include?("No se ha introducido ningun usuario o es incorrecto"), "Tiene que haber un mensaje informativo"
 
 	end	
+	
+	it "Comprueba la carga del logo de la web" do
+		get '/'
+		assert last_response.body.include?("/icons/"+@imagen), "Debe tener el logo de la web" 
+	end
+
+	it "Comprueba si esta el CSS en el servidor" do
+		assert File.exists?(@css), "Debe estar el CSS en el servidor"
+	end
 
 end
 
@@ -111,6 +130,8 @@ describe "Test de la pagina ('json/usuario')" do
 
 
 end
+
+# encoding: UTF-8
 
 
 
